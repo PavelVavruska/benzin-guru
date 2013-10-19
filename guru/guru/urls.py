87bbcae9b791f009d711b7b8f04eadd371f98f5d
Pavel Vavruska <vavruska.pavel@gmail.com>
2013-10-19 12:05:16 +0200

    Add user registration and refactor of url links

--- guru/guru/urls.py
+++ guru/guru/urls.py
@@ -11,12 +11,14 @@ urlpatterns = patterns('',
     # Uncomment the admin/doc line below to enable admin documentation:
     # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
 
-
+    # user auth urls
     url(r'^admin/', include(admin.site.urls)),
-    url(r'^accounts/login/$', 'guru.views.login'),
-    url(r'^accounts/auth/$', 'guru.views.auth_view'),
-    url(r'^accounts/logout/$', 'guru.views.logout'),
-    url(r'^accounts/loggedin/$', 'guru.views.loggedin'),
-    url(r'^accounts/invalid/$', 'guru.views.invalid_login'),
+    url(r'^accounts/login/$', 'guru.views.login', name='login'),
+    url(r'^accounts/auth/$', 'guru.views.auth_view', name='auth_view'),
+    url(r'^accounts/logout/$', 'guru.views.logout', name='logout'),
+    url(r'^accounts/loggedin/$', 'guru.views.loggedin', name='loggedin'),
+    url(r'^accounts/invalid/$', 'guru.views.invalid_login', name='invalid_login'),
+    url(r'^accounts/register/$', 'guru.views.register_user', name='register_user'),
+    url(r'^accounts/register_success/$', 'guru.views.register_success', name='register_success'),
 
 )
