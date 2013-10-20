76309a047a7546660e9702e0bb60fd555c7f156c
Pavel Vavruska <vavruska.pavel@gmail.com>
2013-10-20 09:55:42 +0200

    Add gas prices to detail view of gas station

--- guru/fuel/views.py
+++ guru/fuel/views.py
@@ -1,5 +1,4 @@
 from django.shortcuts import get_object_or_404, render
-from django.core.urlresolvers import reverse
 from .models import GasStation, GasPrice
 
 
@@ -11,4 +10,11 @@ def index(request):
 
 def detail(request, gas_station_id):
     gas_station = get_object_or_404(GasStation, pk=gas_station_id)
-    return render(request, 'fuel/detail.html', {'gas_station': gas_station})
+    gas_prices = GasPrice.objects.filter(station=gas_station).order_by('-date')
+    if gas_prices:
+        gas_prices_js = gas_prices[0].price  # newest gas price for this gas station
+    else:
+        gas_prices_js = ' - Sorry, no gas prices are available.'
+    return render(request, 'fuel/detail.html', {'gas_station': gas_station,
+                                                'gas_prices': gas_prices,
+                                                'gas_prices_js': gas_prices_js})
