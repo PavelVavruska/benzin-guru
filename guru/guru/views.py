a32b4379d0d55fdaf223b9ea5754f83ecc90eb55
Pavel Vavruska <vavruska.pavel@gmail.com>
2013-10-20 10:15:56 +0200

    Refactoring of account templates and main views.

--- guru/guru/views.py
+++ guru/guru/views.py
@@ -8,7 +8,7 @@ from django.contrib.auth.forms import UserCreationForm
 def login(request):
     c = {}
     c.update(csrf(request))
-    return render_to_response('login.html', c)
+    return render_to_response('accounts/login.html', c)
 
 
 def auth_view(request):
@@ -24,16 +24,16 @@ def auth_view(request):
 
 
 def loggedin(request):
-    return render_to_response('loggedin.html',
-                             {'full_name': request.user.username})
+    return render_to_response('accounts/loggedin.html', {'full_name': request.user.username})
+
 
 def invalid_login(request):
-    return render_to_response('invalid_login.html')
+    return render_to_response('accounts/invalid_login.html')
 
 
 def logout(request):
     auth.logout(request)
-    return render_to_response('logout.html')
+    return render_to_response('accounts/logout.html')
 
 
 def register_user(request):
@@ -45,11 +45,9 @@ def register_user(request):
 
     args = {}
     args.update(csrf(request))
-
     args['form'] = UserCreationForm()
+    return render_to_response('accounts/register.html', args)
 
-    return render_to_response('register.html', args)
 
 def register_success(request):
-
-    return render_to_response('register_success.html')
+    return render_to_response('accounts/register_success.html')
