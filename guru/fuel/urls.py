a802135b89a730eff52e5b4f3f64eae360b205c2
Pavel Vavruska <vavruska.pavel@gmail.com>
2013-10-24 22:52:50 +0200

    Loose coupling for urls

--- guru/fuel/urls.py
+++ guru/fuel/urls.py
@@ -4,8 +4,13 @@ from . import views
 
 urlpatterns = patterns('',
     # ex: /guru/
-    url(r'^$', views.index, name='index'),
+    url(regex=r'^$',
+        view=views.index,
+        name='index'),
+
     # ex: /guru/1/
-    url(r'^(?P<gas_station_id>\d+)/$', views.detail, name='detail'),
+    url(regex=r'^(?P<gas_station_id>\d+)/$',
+        view=views.detail,
+        name='detail'),
 )
 
